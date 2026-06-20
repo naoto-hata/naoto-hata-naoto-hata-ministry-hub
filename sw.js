f6a90a2630c99b13fa22e3b0d9ab131155a41a2e
naoto-hata <naoto4236@gmail.com>
2026-06-20 17:18:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,2 @@
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))))});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});self.addEventListener('fetch',e=>{});
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim())));
